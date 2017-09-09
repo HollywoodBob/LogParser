f4cf634fd9bb0a31b799e7c9c7f3bd350828fe97
HollywoodBob <bob.jacobs.pdx@gmail.com>
2017-09-08 20:00:20 -0700

    first checkin

---
+++ cre_index.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+curl -XPUT $ES/sessions?pretty -H 'Content-Type: application/json' -d'
+{
+  "mappings": {
+    "logEntry": {
+      "properties": {
+        "ipAddr": { "type": "ip" },
+        "auth": { "type": "text" },
+        "timestamp": { "type": "date",
+                      "format": "dd/MMM/YYYY:HH:mm:ss Z"
+        },
+        "startTime": { "type": "date",
+                       "format": "epoch_second"
+        },
+        "endTime": { "type": "date",
+                       "format": "epoch_second"
+        },
+        "cmd": { "type": "text" },
+        "statusCode": { "type": "text" },
+        "numBytesSent": { "type": "long" },
+        "referer": { "type": "text" },
+        "userAgent": { "type": "text" },
+        "connectTimeInMinutes": { "type": "long" },
+        "numDurationTime": { "type": "long" },
+        "location": { "type": "geo_point"},
+        "country_iso": {"type": "text"}
+      }
+    },
+    "listenerCountEntry": {
+      "properties": {
+        "lcTimestamp": { "type": "date",
+                       "format": "epoch_second"
+        },
+        "listenerCount": { "type": "long"}
+      }
+    }
+  }
+}
+'
