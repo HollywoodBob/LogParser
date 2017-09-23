29b119a2f34883525ef29be64a4d2551dc173a60
HollywoodBob <bob.jacobs.pdx@gmail.com>
2017-09-22 20:00:58 -0700

    many changes for restructuring

---
+++ batch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170116_185845 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170207_041912 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170227_145221 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170321_054815 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170409_224459 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170506_031758 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170521_143038 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170607_004052 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170628_025418 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170711_105640 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170803_093448 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170815_203553 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170826_022714 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170907_124104 output.log
+python LogParser.py -e 104.199.119.231:9200 -i index_2017 logs/access.log.20170921_153740 output.log
